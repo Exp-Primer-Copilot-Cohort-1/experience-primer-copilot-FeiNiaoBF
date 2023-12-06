820a4cefc88aa39437605701d8757f0f422779f3
Chunzhen Yang <91862060+FeiNiaoBF@users.noreply.github.com>
2023-12-06 08:25:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    scope: {
+      member: '='
+    }
+  };
+}
+
+// Path: member.js
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/skills-member.html',
+        scope: {
+        member: '='
+        }
+    };
+}
